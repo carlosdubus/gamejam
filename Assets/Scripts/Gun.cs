e372bedba26b100b27f2615f6b201e3cbc781195
Carlos Dubus <carlos.garciadm@gmail.com>
2018-01-27 19:30:23 -0400

    gun reload

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -8,20 +8,37 @@ public class Gun : MonoBehaviour {
     public float bulletSpeed = 10f;
     public OVRInput.Button fireButton;
     public int charge;
+    public int maxAmmo = 5;
+    public float reloadTime = 1f;
+
+    public int ammo;
+    float cooldown = 0f;
 
 	// Use this for initialization
 	void Start () {
-		
+        ammo = maxAmmo;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if(OVRInput.GetDown(fireButton)) {
+        cooldown -= Time.deltaTime;
+        if (cooldown <= 0f) {
+            ammo += 1;
+            cooldown = reloadTime;
+        }
+        ammo = Mathf.Clamp(ammo, 0, maxAmmo);
+
+        if(OVRInput.GetDown(fireButton) || Input.GetButtonDown("Fire1")) {
             Fire();
         }
 	}
 
     void Fire() {
+        if (ammo <= 0) {
+            return;
+        }
+        cooldown = reloadTime;
+        ammo -= 1;
         var bullet = (Instantiate(this.bullet, transform.position, Quaternion.identity) as GameObject).GetComponent<Bullet>();
         
         bullet.speed = bulletSpeed;
