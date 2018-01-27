85d12f86733e486bd5055927ecd9c61afaad1a65
Carlos Dubus <carlos.garciadm@gmail.com>
2018-01-27 03:13:06 -0400

    spawner

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public GameObject target;
+    public float speed = 20f;
+    public float charge = 1f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(target != null) {
+            transform.position = Vector3.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
+            if(transform.position == target.transform.position) {
+                Destroy(gameObject);
+            }
+        }
+	}
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    public GameObject player;
+    public float rate = 2f;
+    float cooldown = 0;
+
+    public List<GameObject> enemies = new List<GameObject>();
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        cooldown -= Time.deltaTime;
+        if (cooldown <= 0f) {
+            Spawn();
+            cooldown = rate;
+        }
+	}
+
+    void Spawn(){
+        var prefab = enemies[Random.Range(0, enemies.Count)];
+        var pos = player.transform.position + Random.insideUnitSphere * 30f;
+        pos.y = Mathf.Abs(pos.y);
+        pos.y = Mathf.Clamp(pos.y, 0f, 4f);
+
+        var enemy = Instantiate(prefab, pos, Quaternion.identity) as GameObject;
+        enemy.GetComponent<Enemy>().target = player;
+    }
+}
