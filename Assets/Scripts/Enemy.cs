d8bf6d50041f4dd6700b9f80a5d1c3236fef0312
Carlos Dubus <carlos.garciadm@gmail.com>
2018-01-27 20:48:54 -0400

    enemy sin movement

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,20 +7,30 @@ public class Enemy : MonoBehaviour {
     public GameObject target;
     public float speed = 20f;
     public int charge;
+    public float sinFrequency = 5f;
+    public float sinMagnitude = 1f;
+    public Vector3 sinAxis = Vector3.forward;
+
+    Vector3 pos;
 
 	// Use this for initialization
 	void Start () {
-		
+        pos = transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if(target != null) {
-            transform.LookAt(target.transform);
-            transform.position = Vector3.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
-            if(Vector3.Distance(transform.position, target.transform.position) < 1f) {
-                Destroy(gameObject);
-            }
+        if(target == null) {
+            return;
+        }
+        var direction = (target.transform.position - transform.position).normalized;
+        var oldPos = transform.position;
+        pos += direction * speed * Time.deltaTime;
+        transform.position = pos + sinAxis * Mathf.Sin(Time.time * sinFrequency) * sinMagnitude;
+        transform.forward = (transform.position - oldPos).normalized;
+
+        if(Vector3.Distance(transform.position, target.transform.position) < 1f) {
+            Destroy(gameObject);
         }
 	}
 
